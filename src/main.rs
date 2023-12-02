0d1a6f7bee6c5f2087115116449e18cd704333af
Sébastien <sebmarichal@gmail.com>
2023-12-02 15:50:30 +0100

    Add asteroid

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
 
+mod asteroid;
 mod movement;
 mod player;
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(player::PlayerPlugin)
+        .add_plugins(asteroid::AsteroidPlugin)
         .run();
 }
